1a79c0f05c04a998d62babe14d4ba0ceb0b75dfb
AwesomePresent <yoursmile128@hotmail.com>
2019-08-09 08:43:55 -0700

    Updates

---
+++ precache-manifest.e9577c95ee8ee87afd01aed33346ec41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3df06e2ecabc50c2389fd167ae48c048",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81a32640e557c3d03b7f",
+    "url": "/static/css/main.43c2c563.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "81a32640e557c3d03b7f",
+    "url": "/static/js/main.ef05a95a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
